a8b55ae3d2fee3c3c929e14ee0ca85b91f9e4b6a
Gustavo-Henrique-Ferreira-Alves <gustavohferreiraalves@gmail.com>
2024-04-20 20:30:58 -0300

    feat: Exercicio Two Sum - Força bruta e Hash Map

---
+++ Two Sum/Força Bruta - Brute Force/Força Bruta - Brute Force/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Força_Bruta___Brute_Force
+{
+    class Program
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            int[] numeros = { 1, 2 };
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (target == nums[i] + nums[j])
+                    {
+                        numeros[0] = i;
+                        numeros[1] = j;
+                        break;
+                    };
+                };
+            };
+
+            return numeros;
+        }
+
+
+        static void Main(string[] args)
+        {
+            int[] resultado = TwoSum(new int[] { 2, 3, 4, 5 }, 6);
+            Console.WriteLine($"{resultado[0]}, {resultado[1]}");
+        }
+    }
+}
